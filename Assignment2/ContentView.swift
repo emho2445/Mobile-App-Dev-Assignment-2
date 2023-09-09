b81060cfe646a5a43033b19611cbecf0ab3539af
Emma Hopson <emmaehopson@icloud.com>
2023-09-08 21:39:45 -0600

    Mobile App Assignment 2

--- Assignment2/ContentView.swift
+++ Assignment2/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var switched: Bool = false
+    @State var switchedtwo: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            
+            LinearGradient(colors: [.pink, .purple], startPoint: .topLeading, endPoint: .bottom)
+                
+            VStack{
+                Spacer()
+                    .frame(height: 50.0)
+                Rectangle()
+                    .frame(height: 5.0)
+                
+                Image("IMG_1800")
+                    .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 200, height: 200, alignment: .top)
+                                        .clipShape(Circle())
+                
+                Text("Emma")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                
+                Rectangle()
+                    .frame(height: 5.0)
+                
+                HStack{
+                        
+                        VStack{
+                            //I tried to get the text above the toggle so that it fit but I wasn't able to figure
+                            Toggle("Flip the Switch:", isOn: $switched)
+                                .foregroundColor(Color.white)
+                                .toggleStyle(SwitchToggleStyle(tint: .pink))
+                            if switched{
+                                Text("Button Switched")
+                                    .foregroundColor(Color.indigo)
+                            }
+                        }
+                  
+                        VStack{
+                            Toggle("Flip the Switch:", isOn: $switchedtwo)
+                                .foregroundColor(Color.white)
+                                .toggleStyle(SwitchToggleStyle(tint: .pink))
+                            if switchedtwo{
+                                Image("Boo")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .clipShape(Circle())
+                            }
+                        }
+                }
+            }
+                
+                    
         }
-        .padding()
+        //.background(switched ? Color.orange : Color.purple)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
